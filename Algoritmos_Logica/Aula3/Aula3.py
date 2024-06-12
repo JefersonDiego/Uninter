2e71fa034a66c30c6dd4570b81d5226dad475900
TheRusHBr <the.rush.br@gmail.com>
2024-06-12 16:27:54 -0300

    Exercício de condicionais aninhadas

--- Algoritmos_Logica/Aula3/Aula3.py
+++ Algoritmos_Logica/Aula3/Aula3.py
@@ -99,7 +99,7 @@ print(res)
 # Assuma que a média para aprovação é a partir de 7 e que o aluno cursa 3
 # matérias, somente. Escreva um algoritmo que leia a nota final do aluno em
 # cada matéria e informe, na tela, se ele passou de ano ou não.
-
+'''
 nota1 = float(input("Digita a nota da primeira matéria: "))
 nota2 = float(input("Digita a nota da segunda matéria: "))
 nota3 = float(input("Digita a nota da terceira matéria: "))
@@ -110,3 +110,38 @@ if ( (nota1 >= 7) and (nota2 >=7) and (nota3) >=7 ):
     print('Passou de ano.')
 else:
     print('Não passou de ano.')
+'''
+
+# Condicionais aninhadas
+
+# Escreva um algoritmo em Python em que o usuário escolhe se quer comprar maçãs,
+# laranjas ou bananas. Deverá ser apresentado na tela um menu com as opções:
+# 1 para maçã, 2 para laranja e 3 para banana.
+# Após escolhida a fruta, deve-se digitar quantas unidades se quer comprar. O
+# algoritmo deve calcular o preço total a pagar pelo produto escolhido e mostrá-lo
+# na tela.
+# Considere que uma maçã custa R$2,30, uma laranja, R$3,60 e uma banana, 1,85
+
+maca = 2.3
+laranja = 3.6
+banana = 1.85
+quantidade = 0
+opcao = int(input('Digite a opção que deseja comprar conforme menu abaixo:\n1. Maçã (R$2,30)\n2. Laranja(R$3,60\n3. Banana(R$1,85)\n'))
+quantidade = int(input('Quantas unidades deseja comprar? '))
+
+if (opcao == 1):
+    preco_total = quantidade * maca
+    produto_existe = 1
+elif (opcao == 2):
+    preco_total = quantidade * laranja
+    produto_existe = 1
+elif (opcao == 3):
+    preco_total = quantidade * banana
+    produto_existe = 1
+else:
+    produto_existe = 0
+
+if (produto_existe == 1):
+    print('Valor total a pagar R${0:.3g}.'.format(preco_total))
+elif (produto_existe != 1):
+    print('Produto não existe.')

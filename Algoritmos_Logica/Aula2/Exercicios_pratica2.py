3d2ccb3acced1c36c3fee82472a89b35c9482b79
TheRusHBr <the.rush.br@gmail.com>
2024-06-12 10:03:47 -0300

    Criação do arquivo Exercicios_pratica2, finalização do exercício 1.

---
+++ Algoritmos_Logica/Aula2/Exercicios_pratica2.py
@@ -0,0 +1,13 @@
+# Exercício 1
+# Desenvolva um algoritmoo que solicite ao usuário o preço de um produto
+# e um percentual de desconto a ser aplicado a ele. Calcule e exiba o
+# valor do desconto e o preço final do produto.
+
+preco_produto = float(input('Digite o valor do seu produto: R$'))
+percentual_desconto = float(input('Digite o percentual de desconto:    %\b\b\b\b'))
+
+valor_desconto = (preco_produto * percentual_desconto) / 100
+preco_final = preco_produto - valor_desconto
+
+print('O valor do seu desconto é de R${}'.format(valor_desconto))
+print('O preço final do seu produto é de R${}'.format(preco_final))

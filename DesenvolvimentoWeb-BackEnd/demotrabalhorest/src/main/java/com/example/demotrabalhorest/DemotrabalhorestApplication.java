88e69d00e80183b154cce4c4c83cd0ea1e3c02cf
TheRusHBr <the.rush.br@gmail.com>
2025-11-24 11:23:33 -0300

    Adicionando projeto demotrabalhorest

---
+++ DesenvolvimentoWeb-BackEnd/demotrabalhorest/src/main/java/com/example/demotrabalhorest/DemotrabalhorestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotrabalhorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotrabalhorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotrabalhorestApplication.class, args);
+	}
+
+}
---
+++ DesenvolvimentoWeb-BackEnd/demotrabalhorest/src/test/java/com/example/demotrabalhorest/DemotrabalhorestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotrabalhorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotrabalhorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
